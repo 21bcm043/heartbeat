57415738f0604c09e5f0da6ce163c02e95472b2a
21bcm043 <159792906+21bcm043@users.noreply.github.com>
2024-02-15 09:04:28 +0530

    Update contents.ts

--- config/contents.ts
+++ config/contents.ts
@@ -13,47 +13,104 @@ export const heroHeader: HeroHeader = {
   image: `/Screenshot.png`,
 }
 
-export const featureCards: ContentSection = {
-  header: `Event Guidelines`,
-  subheader: `Here are the guidelines that you need to follow`,
+export const offStageEvents: ContentSection = {
+  header: `Off Stage Event Guidelines`,
+  subheader: `Here are the guidelines that you need to follow for the off-stage events`,
   content: [
     {
-      text: `Respectful Behavior`,
-      subtext: `Treat all event attendees, organizers, and speakers with respect and courtesy. Avoid disruptive behavior such as talking loudly, using electronic devices inappropriately, or causing disturbances that may detract from others' enjoyment of the event.`,
-      icon: "respectfulBehavior",
+      "text": "20/02/2024 Rang Rashmi (Rangoli)",
+      "subtext": "Participants permitted: 1 team per department (3 members in a team).\nParticipants must bring the requirements (Rangoli Powder).\nParticipants should not use mobile phones during the competition.\nTheme will be given on the spot and places will be allotted.\nThe decision of the judges will be final.\nDuration: 2 hours."
     },
     {
-      text: `Follow Instructions:`,
-      subtext: `Adhere to any instructions or guidelines provided by event organizers, including seating arrangements, designated entry and exit points, and safety protocols. Following these instructions helps ensure a smooth and enjoyable experience for everyone involved.`,
-      icon: "followInstructions",
+      "text": "20/02/2024 Rhyme Riot (Poetry Competition)",
+      "subtext": "Participants permitted: 2 individual participants per department.\nMedium of poem writing: English or Tamil.\nParticipant can submit only one poem on A3/A4 sheet with minimum 10 lines.\nPoetry must be on the given topic/theme.\nTheme of the poem will be given on spot.\nThe decision of the judges will be final.\nDuration: 45 minutes."
     },
     {
-      text: `Participation:`,
-      subtext: `Engage actively in event activities, such as competitions, or discussions, by asking questions, and participating in group activities. Your involvement contributes to the success of the event and enriches the experience for yourself and others.`,
-      icon: "participation",
+      "text": "20/02/2024 The Loudspeakers (Extempore)",
+      "subtext": "Participants permitted: 2 individual participants per department.\nEach participant shall be given 1-2 minutes to speak on the given topic.\nLanguage used shall be strictly in English or Tamil.\nNo Bilingualism is encouraged.\nAny kind of vulgarity during the competition leads to immediate disqualification.\nThe decision of the judges will be final."
     },
-  ],
+    {
+      "text": "20/02/2024 404 not found (Situational Analysis)",
+      "subtext": "Participants permitted: 1 team per department (2 members in a team).\nParticipants will be given with a situational crisis on the spot.\nParticipants will have to prepare a presentation showcasing a solution to the given crisis within a time duration of 45 minutes.\nParticipants can take internet references and references from the books provided in the library.\nA minimum of 10 slides are to be presented.\nThe decision of the judges will be final.\nDuration: 1 hour 30 minutes (45 minutes for preparation; 45 minutes for presentation)."
+    },
+    {
+      "text": "21/02/2024 No Boil No Oil (Flameless Cooking)",
+      "subtext": "Participants permitted: 1 team per department (2 members in a team).\nParticipants are allowed to prepare one main dish.\nAdditions for the main dish can be provided but it should be prepared by the participants.\nAll the necessary ingredients, requirements for preparation & plating should be brought by the participants."
+    },
+    {
+      "text": "21/02/2024 Shades of Cheek (Face Painting)",
+      "subtext": "Participants permitted: 2 teams per department (2 members in a team).\nParticipants should bring their own colours, brushes and other requirements.\nParticipants should not use mobile phones during the competition.\nTopic will be given on the spot.\nThe decision of the judges will be final.\nDuration: 1 hour."
+    },
+    {
+      "text": "21/02/2024 Art Buff (Origami, Handicrafts)",
+      "subtext": "Participants permitted: 3 individual participants per department.\nParticipants are supposed to bring their own materials.\nParticipants will be engaged in three rounds consisting of origami work, vegetables or fruits carvings and the final round involving crafts open to their own ideas.\nParticipants should not use any kind of reference for their art work during the competition.\nAny kind of misconduct will lead to immediate dismissal of the participant.\nNo electronic Machinery or Gadgets are allowed for doing art work.\nDuration: 1 hour."
+    },
+    {
+      "text": "21/02/2024 Henna Hues (Mehandi Competition)",
+      "subtext": "Participants permitted: 1 team per department (2 members in a team).\nParticipants must bring the requirements.\nParticipants should not use any kind of reference for doing their mehandi during the competition.\nAny kind of misconduct will lead to immediate dismissal of the participant.\nNo Electronic Gadgets are allowed.\nThe decision of the judges will be final.\nDuration: 45 minutes."
+    },
+    {
+      "text": "22/02/2024 IPL Auction",
+      "subtext": "This is a team event.\nEach team should consist of only 4 (one will be captain) participants.\nOnly one Participant (Team captain) will be allowed to bid in the 2nd round.\nPlease decide one key participant among your teammates before submitting the registration form to enter the team details. Changes will not be entertained.\nThere will be 2 rounds in this event.\nThe first round will be the quiz based on IPL.\nThe 2nd round is the auction round where the teams will bid for the players they want."
+    },
+    {
+      "text": "22/02/2024 Cinefesto (Movie Marathon)",
+      "subtext": "Participants permitted: 1 team per department (3 members in a team).\nParticipants will engage in 3 rounds consisting of a movie quiz, connection round and the redub round.\nParticipants have to re-enact the scene allotted to them in the redub round.\nUsage of Mobile phones are not permitted.\nAny kind of misconduct will lead to immediate dismissal of the team.\nThe decision of the judges will be final."
+    },
+    {
+      "text": "22/02/2024 Untold stories behind the lens (Photography)",
+      "subtext": "Participants permitted: 1 team per department (maximum of 3 members in a team).\nParticipants should use only their phone for the photography.\nParticipants have to frame story/message with the photos taken.\nPhotography is allowed only within the campus.\nAny kind of vulgarity during the competition leads to immediate disqualification of team.\nThe decision of the judges will be final.\nDuration: 1 hour 30 minutes."
+    },
+    {
+      "text": "22/02/2024 AD Blitz (Instant Ad Creation)",
+      "subtext": "Participants permitted: 1 team per department (3 members in a team).\nParticipants will have to create an ad using any applications in the mobile phone based on the given theme.\nDuration of the ad should be limited to 20-30 seconds.\nThe decision of the judges will be final."
+    },
+    {
+      "text": "22/02/2024 Brushstroke (Painting/Pencil Sketching)",
+      "subtext": "Participants permitted: One individual participant per department.\nParticipants must bring the requirements (A4/A3 sheet and other stationery items required).\nTheme will be given on the spot.\nThe painting/ pencil sketching should be based on the given theme.\nThe decision of the judges will be final.\nDuration: 45 minutes."
+    },
+    {
+      "text": "23/02/2024 Expenses Explorer (Budget)",
+      "subtext": "Participants permitted: 2 members in a team.\nParticipants will engage in 3 rounds.\nUsage of Mobile phones are not permitted.\nAny kind of misconduct will lead to immediate dismissal of the team.\nThe decision of the judges will be final."
+    },
+    {
+      "text": "23/02/2024 Master Mind’s Match (Quiz)",
+      "subtext": "Participants permitted: 1 team per department (3 members in a team).\nThe medium of questions will only be in English.\nQuiz Questions will be based on the General Topics.\nPrelims will be conducted based on number of registrations.\nWill be conducted at various levels."
+    },
+    {
+      "text": "23/02/2024 Puzzle Pirates (Puzzle Relay)",
+      "subtext": "Participants permitted: 1 team per department (3 members in a team).\nParticipants are not allowed to use mobile phones during the competition.\nParticipants will be involved in 3 rounds solving Rebus, Word and Jigsaw puzzle.\nAny kind of conduct by the participants will result in immediate disqualification.\nThe decision of the judges will be final."
+    },
+    {
+      "text": "23/02/2024 Tamilodu Vilayadu",
+      "subtext": "Participants permitted: 1 team per department (2 members in a team).\nParticipants must know to read and write Tamil.\nParticipants should have a basic knowledge about Tamil literature and language.\nFull event is completely organized in Tamil.\nDuration: 2 hours."
+    }
+  ]
 }
 
-export const features: ContentSection = {
-  header: `Features`,
-  subheader: `Why use Next Landing?`,
-  image: `/features-img.webp`,
+export const onStageEvents: ContentSection = {
+  header: `On Stage Event Guidelines`,
+  subheader: `Here are the guidelines that you need to follow for the on-stage events`,
   content: [
     {
-      text: `SEO Optimized`,
-      subtext: `Improved website visibility on search engines`,
-      icon: "fileSearch",
+      "text": "Beatbox (Solo Dance)",
+      "subtext": "Participants permitted: 2 individual participants per department.\nOnly pendrive is allowed.\nAvoid obscene songs and costumes.\nThe decision of the judges will be final.\nDuration: 4 minutes."
     },
     {
-      text: `Highly Performant`,
-      subtext: `Fast loading times and smooth performance`,
-      icon: "barChart",
+      "text": "Dance-A-Thon (Group Dance)",
+      "subtext": "Participants permitted: 1 team per department.\nOnly pen drive is allowed.\nAvoid obscene songs and costumes.\nThe decision of the judges will be final.\nDuration: 4 minutes."
     },
     {
-      text: `Easy Customizability`,
-      subtext: `Change your content and layout with little effort`,
-      icon: "settings",
+      "text": "Rhythm of Voice (Solo Singing)",
+      "subtext": "Participants permitted: 2 individual participants per department.\nChoice of song is open to the participant but the song should not have any slangs or derogatory language.\nNo karaoke allowed.\nThe decision of the judges will be final.\nDuration: 2 minutes."
     },
-  ],
+    {
+      "text": "PSGCAS Icon (Variety Entertainment)",
+      "subtext": "Participants permitted: 2 entries per department (Either individual participation or a team of maximum of 8 members).\nAny kind of talents can be expressed on the stage.\nAvoid obscene content.\nThe decision of the judges will be final.\nDuration: 3 minutes."
+    },
+    {
+      "text": "Groove to the beat (Adapt Tunes)",
+      "subtext": "Participants permitted: 2 entries per department.\nParticipants have to dance adapting to the songs played on the spot.\nThe decision of the judges will be final.\nDuration: 2 minutes."
+    }
+  ]
 }
