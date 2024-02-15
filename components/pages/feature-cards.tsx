fce2a6075fdf3056840170ebbfc02011f42461c2
21bcm043 <159792906+21bcm043@users.noreply.github.com>
2024-02-15 09:03:09 +0530

    Update feature-cards.tsx

--- components/pages/feature-cards.tsx
+++ components/pages/feature-cards.tsx
@@ -1,34 +1,56 @@
 import Image from "next/image"
 import HeadingText from "@/components/heading-text"
-import { featureCards } from "@/config/contents"
+import { offStageEvents, onStageEvents } from "@/config/contents"
 import { Card, CardDescription, CardTitle } from "@/components/ui/card"
 import { Icons } from "@/components/icons"
 import Video from "../ui/video"
+import AnimatedText from "../ui/alternating-text"
 
 export default function FeatureCards() {
   return (
     <section className="bg-slate-50 dark:bg-slate-900" id="guidelines">
       <div className="container space-y-8 py-12 text-center lg:py-20">
-        {featureCards.header || featureCards.subheader ? (
-          <HeadingText subtext={featureCards.subheader}>
-            {featureCards.header}
+        <AnimatedText text="On stage Events" />
+        {onStageEvents.header || onStageEvents.subheader ? (
+          <HeadingText subtext={onStageEvents.subheader}>
+            {onStageEvents.header}
           </HeadingText>
         ) : null}
         <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
-          {featureCards.content.map((cards) => {
-            const Icon = Icons[cards.icon || "blank"]
+          {onStageEvents.content.map((cards) => {
 
             return (
               <Card
                 key={cards.text}
                 className="flex flex-grow flex-col items-center justify-between gap-4 p-8 dark:bg-secondary"
               >
-                <div className="flex">
-                  <Icon className="h-[6rem] w-[6rem]" />
-                </div>
                 <div className="space-y-2">
                   <CardTitle>{cards.text}</CardTitle>
-                  <CardDescription>{cards.subtext}</CardDescription>
+                  <CardDescription style={{ whiteSpace: 'pre-line' }}>{cards.subtext}</CardDescription>
+                </div>
+              </Card>
+            )
+          })}
+        </div>
+      </div>
+      <div className="container space-y-8 py-12 text-center lg:py-20">
+        <AnimatedText text="Off stage Events" />
+        {offStageEvents.header || offStageEvents.subheader ? (
+          <HeadingText subtext={offStageEvents.subheader}>
+            {offStageEvents.header}
+          </HeadingText>
+        ) : null}
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+          {offStageEvents.content.map((cards) => {
+
+            return (
+              <Card
+                key={cards.text}
+                className="flex flex-grow flex-col items-center justify-between gap-4 p-8 dark:bg-secondary"
+              >
+                <div className="space-y-2">
+                  <CardTitle>{cards.text}</CardTitle>
+                  <CardDescription style={{ whiteSpace: 'pre-line' }}>{cards.subtext}</CardDescription>
                 </div>
               </Card>
             )
