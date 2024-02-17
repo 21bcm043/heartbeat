447811b4148c47c4bc32f96054b7d6f01299dc48
21bcm043 <159792906+21bcm043@users.noreply.github.com>
2024-02-17 22:23:03 +0530

    Update video.tsx

--- components/ui/video.tsx
+++ components/ui/video.tsx
@@ -1,3 +1,5 @@
+
+"use client";
 import React from "react";
 
 type VideoProps = {
@@ -5,17 +7,25 @@ type VideoProps = {
     width?: string; // Optional width of the video player
     height?: string; // Optional height of the video player
 };
+import ReactPlayer from "react-player";
 
-const VideoPlayer: React.FC<VideoProps> = ({ src, width = "100%", height = "auto" }) => {
+const VideoPlayer = ({ src, width = "500px", height = "400px" }: VideoProps) => {
+    //video path
+
     return (
-        <video
-            controls autoPlay loop muted
-            width={width}
-            height={height}
-            className="rounded-xl "
-        >
+        <div>
+            <ReactPlayer
+                width={width}
+                height={height}
+                url={src}
+                controls={true}
+                // light is usefull incase of dark mode
+                light={false}
+                // picture in picture
+                pip={true}
+            />
             <source src={src} type="video/mp4" />
-        </video>
+        </div>
     );
 };
 
