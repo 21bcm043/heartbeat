5bdf43008c89d3d3c3d77af3f9bb96e3ad95f668
21bcm043 <159792906+21bcm043@users.noreply.github.com>
2024-02-17 22:24:00 +0530

    Update hero.tsx

--- components/pages/hero.tsx
+++ components/pages/hero.tsx
@@ -1,31 +1,34 @@
-"use client";
-import React from "react";
+import Link from "next/link"
+import { Button, buttonVariants } from "@/components/ui/button"
+import { heroHeader } from "@/config/contents"
+import VideoPlayer from "../ui/video"
 
-type VideoProps = {
-    src: string; // Path to the video file in the public folder
-    width?: string; // Optional width of the video player
-    height?: string; // Optional height of the video player
-};
-import ReactPlayer from "react-player";
-
-const VideoPlayer = ({ src, width = "500px", height = "400px" }: VideoProps) => {
-    //video path
-
-    return (
-        <div>
-            <ReactPlayer
-                width={width}
-                height={height}
-                url={src}
-                controls={true}
-                // light is usefull incase of dark mode
-                light={false}
-                // picture in picture
-                pip={true}
-            />
-            <source src={src} type="video/mp4" />
+export default function HeroHeader() {
+  return (
+    <section className="container flex flex-col gap-4 pb-12 pt-4 text-center lg:items-center lg:gap-8 lg:py-20">
+      <div className="flex flex-1 flex-col items-center gap-4 text-center lg:gap-8">
+        <div className="space-y-4">
+          <h1 className="text-4xl font-bold lg:text-6xl duration-200 hover:text-amber-400 lg:hover:scale-[1.10]">
+            {heroHeader.header}
+          </h1>
+          <h2 className="text-lg font-light text-foreground lg:text-3xl">
+            {heroHeader.subheader}
+          </h2>
+          <h2 className="text-lg font-bold text-foreground lg:text-3xl duration-200 hover:text-amber-400 lg:hover:scale-[1.10]">
+            Date: 1/3/2024
+          </h2>
         </div>
-    );
-};
-
-export default VideoPlayer;
+        <Button className="bg-amber-400 duration-200 lg:hover:scale-[1.10]"><Link
+          href="/register"
+          className={`py-4 font-bold text-xl`}
+        >
+          Register Now!!!
+        </Link></Button>
+        <p className="font-light text-sm text-slate-300">{"Scroll Down👇 for Guidelines"}</p>
+      </div>
+      <div className="flex flex-1 justify-center lg:justify-end">
+        <VideoPlayer src="/heartbeat_vid.mp4" height={"240"} width={"640"} />
+      </div>
+    </section>
+  )
+}
